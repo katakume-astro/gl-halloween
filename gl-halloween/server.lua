1200d88e921c307b725b4ad86746a50a3996d92a
katakume <72819680+katakume-astro@users.noreply.github.com>
2022-10-02 12:42:30 +0200

    Update server.lua

--- gl-halloween/server.lua
+++ gl-halloween/server.lua
@@ -1,10 +1,10 @@
 RegisterServerEvent('gl-halloween:getSurprise', function() -- all sv edited by katakume
 	local xPlayer = ESX.GetPlayerFromId(source)
-	local surprise = Config.Items[math.random(#Config.Items)]
+	local niespodzianka = Config.Items[math.random(#Config.Items)]
 	local szescie = math.random(1, 2)
 
 	if szescie <= 1 then
-		xPlayer.addInventoryItem(surprise, Config.Amount)
+		xPlayer.addInventoryItem(nieszpodzianka, Config.Amount)
 	else
 		xPlayer.showNotification('Osoba nie ma cukierkow ale w ramach rekompensaty dostales troche szmalu')
 		xPlayer.addMoney(szescie)
